1b9e0b34116a4051a27dffbe1dece82e0983ed45
Nil Martinez <nilpazgavilan@gmail.com>
2026-03-23 15:49:56 +0100

    Hola

--- backend/db/dbClient.js
+++ backend/db/dbClient.js
@@ -1,26 +1,28 @@
 // Importamos las dependecias
-import 'dotenv/config'
-import mongoose from 'mongoose'
+import "dotenv/config";
+import mongoose from "mongoose";
 
 // Creamos la clase dbClient que se encarga de la conexión
 class dbClient {
     constructor() {
-        console.log("Funcionando")
-        this.conectarBaseDatos()
+        console.log("Funcionando");
+        this.conectarBaseDatos();
     }
 
     // Método para conectar a la base de datos
     async conectarBaseDatos() {
         const user = encodeURIComponent(process.env.USER_DB);
         const pass = encodeURIComponent(process.env.PWD_DB);
-        const queryString = `mongodb+srv://${user}:${pass}@${process.env.SERVER_DB}/?appName=GestorDePermisosLaborals`
+        const queryString = `mongodb+srv://${user}:${pass}@${process.env.SERVER_DB}/?appName=GestorDePermisosLaborals`;
         try {
-            await mongoose.connect(queryString)
+            await mongoose.connect(queryString);
             console.log("✅ Conectado a la base de datos");
         } catch (e) {
             console.error("❌ Error de conexión a la base de datos:");
-            if (e.message.includes('Authentication failed')) {
-                console.error("   Parece un error de credenciales (usuario o contraseña incorrectos en .env)");
+            if (e.message.includes("Authentication failed")) {
+                console.error(
+                    "   Parece un error de credenciales (usuario o contraseña incorrectos en .env)",
+                );
             } else {
                 console.error("  ", e.message);
             }
@@ -30,11 +32,10 @@ class dbClient {
     // Método para cerrar la conexión
     async cerrarConexion() {
         try {
-            await mongoose.disconnect()
+            await mongoose.disconnect();
             console.log("Conexión a la base de datos cerrada");
         } catch (e) {
             console.log("Error al cerrar la conexión:", e);
-
         }
     }
 }
