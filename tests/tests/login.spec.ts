04b517c602a2b8b0291c1a831d81b5e777237288
Nil Martinez <nilpazgavilan@gmail.com>
2026-05-06 16:13:14 +0200

    ª

--- tests/tests/login.spec.ts
+++ tests/tests/login.spec.ts
@@ -7,9 +7,9 @@ test("test login", async ({ page }) => {
     // 2. Interactuar (Localizar un botón por su texto y clicar)
     await page.getByRole("button", { name: "Sign in" }).click();
     // 3. Verificar (Localizar un elemento por su texto y verificar que es visible)
-    await page.getByLabel("User Name").fill("Admin");
-    await page.getByLabel("Password").fill("Admin1234");
-    await page.getByRole("button", { name: "Sign in" }).click();
+    await page.getByLabel("Nom d'usuari").fill("Admin");
+    await page.getByLabel("Contrasenya").fill("Admin1234");
+    await page.getByRole("button", { name: "Inicia sessió" }).click();
     await expect(page).toHaveURL(/admin/);
 });
 test("Wrong Password", async ({ page }) => {
@@ -19,12 +19,12 @@ test("Wrong Password", async ({ page }) => {
     // 2. Interactuar (Localizar un botón por su texto y clicar)
     await page.getByRole("button", { name: "Sign in" }).click();
     // 3. Verificar (Localizar un elemento por su texto y verificar que es visible)
-    await page.getByLabel("User Name").fill("Admin");
-    await page.getByLabel("Password").fill("Error");
+    await page.getByLabel("Nom d'usuari").fill("Admin");
+    await page.getByLabel("Contrasenya").fill("Error");
     // Esperar al diálogo de alerta que se dispara al hacer click
     const [dialog] = await Promise.all([
         page.waitForEvent("dialog"),
-        page.getByRole("button", { name: "Sign in" }).click(),
+        page.getByRole("button", { name: "Inicia sessió" }).click(),
     ]);
     expect(dialog.message()).toBe("undefined");
     await dialog.accept();
