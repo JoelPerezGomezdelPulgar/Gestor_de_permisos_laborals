46c5aed03ca2921832a1dbd52793428647077f77
Nil Martinez <nilpazgavilan@gmail.com>
2026-05-11 15:36:28 +0200

    fix(logging): add error logs for unauthorized access due to invalid tokens
    build(frontend): update Dockerfile to use legacy peer dependencies for npm install
    
    build(frontend): downgrade Angular dependencies to version 20.3.0 for compatibility
    
    build(tests): update Angular dependencies in tests to version 20.3.19
    
    test(login): update login test credentials for consistency with new user setup

--- tests/tests/login.spec.ts
+++ tests/tests/login.spec.ts
@@ -7,8 +7,8 @@ test("test login", async ({ page }) => {
     // 2. Interactuar (Localizar un botón por su texto y clicar)
     await page.getByRole("button", { name: "Sign in" }).click();
     // 3. Verificar (Localizar un elemento por su texto y verificar que es visible)
-    await page.getByLabel("User Name").fill("Admin");
-    await page.getByLabel("Password").fill("Admin1234");
+    await page.getByLabel("User Name").fill("Miniboo");
+    await page.getByLabel("Password").fill("1234");
     await page.getByRole("button", { name: "Sign in" }).click();
     await expect(page).toHaveURL(/admin/);
 });
