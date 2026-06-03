2fe5e8c84d15a205c231658076937a1fd9a15fec
Nil Martinez <nilpazgavilan@gmail.com>
2026-06-03 16:51:20 +0200

    feat(logging): improve error handling and logging in backend; update login error messages in frontend

--- tests/tests/login.spec.ts
+++ tests/tests/login.spec.ts
@@ -1,5 +1,7 @@
-import { test, expect } from "@playwright/test";
+// cd tests
+// npx playwright test
 
+import { test, expect } from "@playwright/test";
 test("test login", async ({ page }) => {
     // 1. Navegar a tu app (debe estar corriendo)
     await page.goto("http://localhost:4200");
